48f356e99b38fd31fa9a573b3c5f790f03a2f244
james <2066473614@qq.com>
2019-05-03 16:50:26 +0800

    添加购物车小球动画效果+购物车商品数量和numbox同步

--- 07_vue/code/02.VUE-CMS/src/components/goods/GoodsInfo.vue
+++ 07_vue/code/02.VUE-CMS/src/components/goods/GoodsInfo.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="goodsinfo-container">
+    <transition 
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter">
+      <div class="ball" v-show="ballFlag" ref="ball">
+      </div>
+    </transition>
     <div class="mui-card">
       <div class="mui-card-content">
         <div class="mui-card-content-inner">
@@ -15,10 +22,15 @@
             市场价：<del>￥{{ goodsInfo.market_price }}</del>&nbsp;&nbsp;
             销售价：<span class="now_price">￥{{ goodsInfo.sell_price }}</span>
           </p>
-          <p>购买数量：<numbox></numbox></p>
+          <p>购买数量：
+            <numbox 
+              @updateGoodsAmountOfShopCat="updateGoodsAmountOfShopCat"
+              :numboxMax="goodsInfo.stock_quantity">
+            </numbox>
+          </p>
           <p>
             <mt-button type="primary" size="small">立即购买</mt-button>
-            <mt-button type="danger" size="small">加入购物车</mt-button>
+            <mt-button type="danger" size="small" @click="addToShopCat">加入购物车</mt-button>
           </p>
         </div>
       </div>
@@ -41,73 +53,111 @@
 </template>
 
 <script>
-import carousel from "../subcomponents/Carousel.vue"
-import numbox from "../subcomponents/Numbox.vue"
+  import carousel from "../subcomponents/Carousel.vue"
+  import numbox from "../subcomponents/Numbox.vue"
 
-export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-      carouselPics: [],
-      goodsInfo: {}
+  export default {
+    data() {
+      return {
+        id: this.$route.params.id,
+        carouselPics: [],
+        goodsInfo: {},
+        ballFlag: false,
+        goodsAmountOfShopCat: 1
+      }
+    },
+    components: {
+      carousel,
+      numbox
+    },
+    methods: {
+      generateCarouselPics() {
+        this.$http.get("api/getthumimages/" + this.id).then(result => {
+          if (result.body.status == 0) {
+            result.body.message.forEach(item => {
+              item.img = item.src
+            })
+            this.carouselPics = result.body.message
+          }
+        })
+      },
+      generateGoodsInfo() {
+        this.$http.get("api/goods/getinfo/" +this.id).then(result => {
+          if (result.body.status == 0) {
+            this.goodsInfo = result.body.message[0]
+          }
+        })
+      },
+      goDesc() {
+        this.$router.push({name: "goodsDesc", params: {id: this.id}})
+      },
+      goComments() {
+        this.$router.push({name: "goodsComment", params: {id: this.id}})
+      },
+      addToShopCat() {
+        this.ballFlag = !this.ballFlag
+      },
+      updateGoodsAmountOfShopCat(count) {
+        this.goodsAmountOfShopCat = count
+        // console.log(this.goodsAmountOfShopCat)
+      },
+      /**
+       * 因为是半程动画，所以需要使用 钩子函数来实现动画
+       */
+      beforeEnter(el) {
+        el.style.transform = "translate(0, 0)"
+      },
+      enter(el, done) {
+        el.offsetWidth
+        const ballPosition = this.$refs.ball.getBoundingClientRect()
+        const badgePosition = document.getElementById("badge").getBoundingClientRect()
+        const XOffset =  badgePosition.left - ballPosition.left
+        const YOffset =  badgePosition.top - ballPosition.top
+        el.style.transform = `translate(${XOffset}px, ${YOffset}px)`
+        el.style.transition = "all 1s cubic-bezier(.4,-0.3,1,.68)"
+        done()
+      },
+      afterEnter(el) {
+        this.ballFlag = !this.ballFlag
+      }
+    },
+    created() {
+      this.generateCarouselPics()
+      this.generateGoodsInfo()
     }
-  },
-  components: {
-    carousel,
-    numbox
-  },
-  methods: {
-    generateCarouselPics() {
-      this.$http.get("api/getthumimages/" + this.id).then(result => {
-        if (result.body.status == 0) {
-          result.body.message.forEach(item => {
-            item.img = item.src
-          })
-          this.carouselPics = result.body.message
-        }
-      })
-    },
-    generateGoodsInfo() {
-      this.$http.get("api/goods/getinfo/" +this.id).then(result => {
-        if (result.body.status == 0) {
-          this.goodsInfo = result.body.message[0]
-        }
-      })
-    },
-    goDesc() {
-      this.$router.push({name: "goodsDesc", params: {id: this.id}})
-    },
-    goComments() {
-      this.$router.push({name: "goodsComment", params: {id: this.id}})
-    }
-  },
-  created() {
-    this.generateCarouselPics()
-    this.generateGoodsInfo()
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.goodsinfo-container {
-  background-color: #eee;
-  overflow: hidden;
+  .goodsinfo-container {
+    background-color: #eee;
+    overflow: hidden;
 
-  .now_price {
-    font-size: 16px;
-    color: red;
-    font-weight: bold;
-  }
+    .now_price {
+      font-size: 16px;
+      color: red;
+      font-weight: bold;
+    }
 
-  //button的父元素div是flex， 导致button不能换行显示，button中间加上<br/>也不起作用，
-  //只有更换父元素的显示方式为block
-  .mui-card-footer {
-    display: block;
+    //button的父元素div是flex， 导致button不能换行显示，button中间加上<br/>也不起作用，
+    //只有更换父元素的显示方式为block
+    .mui-card-footer {
+      display: block;
 
-    button {
-      margin: 15px 0;
+      button {
+        margin: 15px 0;
+      }
+    }
+    .ball {
+      width: 15px;
+      height: 15px;
+      border-radius: 50%;
+      background-color: red;
+      position: absolute;
+      top: 409px;
+      left: 148px;
+      z-index: 99;
     }
   }
-}
 
 </style>
--- 07_vue/code/02.VUE-CMS/src/components/subcomponents/Numbox.vue
+++ 07_vue/code/02.VUE-CMS/src/components/subcomponents/Numbox.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="mui-numbox" data-numbox-min='1' data-numbox-max='9'>
+  <div class="mui-numbox" data-numbox-min='1'>
     <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
-    <input id="test" class="mui-input-numbox" type="number" value="1" />
+    <input id="test" class="mui-input-numbox" type="number" value="1" ref="numboxVal" 
+      @change="updateGoodsAmountOfShopCat" />
     <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
   </div>  
 </template>
 
 <script>
-import mui from '../../lib/mui/js/mui.min.js'
+  import mui from '../../lib/mui/js/mui.min.js'
 
-export default {
-  mounted() {
-    mui("mui-numbox").numbox() 
-  },
-}
+  export default {
+    props: {
+      numboxMax: Number
+    },
+    watch: {
+      numboxMax: function(newVal) {
+        if (newVal) {
+          mui(".mui-numbox").numbox().setOption('max', newVal)
+        }
+      }
+    },
+    methods: {
+     updateGoodsAmountOfShopCat() {
+       this.$emit('updateGoodsAmountOfShopCat', parseInt(this.$refs.numboxVal.value))
+     }
+    },
+    mounted() {
+      mui(".mui-numbox").numbox()
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
