7d0478dc142e6c5bf4915d877cf6a855e0ed809e
james <2066473614@qq.com>
2019-05-03 17:56:41 +0800

    初始化vuex-study project

---
+++ 07_vue/code/03.VUEX-study/src/components/counter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    counter
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // count: 0
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
